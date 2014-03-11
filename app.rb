e2296c739175128726a32a9dfbb69cec25f7987d
Sam Winter <sam.winter@gmail.com>
2014-03-11 15:06:44 -0400

    Fixed spacing again.

--- app.rb
+++ app.rb
@@ -5,24 +5,24 @@ class App
   attr_accessor :memcachedurl, :name, :cacheclient
 
   def initialize settings
-		@memcachedurl = settings[:memcachedurl]
-		@name = settings[:name]
-		@cacheclient = Dalli::Client.new(@memcachedurl, { 
-			:namespace => @name, 
-			:compress => true 
-			})
+    @memcachedurl = settings[:memcachedurl]
+    @name = settings[:name]
+    @cacheclient = Dalli::Client.new(@memcachedurl, { 
+        :namespace => @name, 
+        :compress => true 
+      })
   end
 
   def call env
-  	request = Rack::Request.new env
-  	response = Rack::Response.new
-  	key = 'woohoo'
-  	value = @cacheclient.get(key)
-  	value = Time.now if value.nil?
-  	puts value
-  	@cacheclient.set(key, value) if value.nil?
+    request = Rack::Request.new env
+    response = Rack::Response.new
+    key = 'woohoo'
+    value = @cacheclient.get(key)
+    value = Time.now if value.nil?
+    puts value
+    @cacheclient.set(key, value) if value.nil?
 
-		response.body= ["Here is the value: |#{value}|"]
-  	response.finish
+    response.body= ["Here is the value: |#{value}|"]
+    response.finish
   end
 end
